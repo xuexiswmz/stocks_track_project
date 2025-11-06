70c32b72198e446fa0dcde418d62187e88e97c7d
xuexiswmz <2635003975@qq.com>
2025-11-06 09:47:30 +0800

    fix: add root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,11 @@
 import React from "react";
-import '../globals.css'
 import Header from "@/components/Header";
-const Layout = ({children}: { children:React.ReactNode }) => {
-    return (
-        <html>
-            <body>
-                <main className="min-h-screen text-gray-400">
-                    <Header/>
-                    <div className="container py-10">{children}</div>
-                </main>
-            </body>
-        </html>
-    )
-}
-export default Layout
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+      <Header />
+      <div className="container py-10">{children}</div>
+    </main>
+  );
+};
+export default Layout;
---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="dark">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
